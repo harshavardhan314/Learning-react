c87ce8876c3e91152761aebcaad87edd614adf7f
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-02 17:33:07 +0530

    Update card.jsx

--- reactapp/src/card.jsx
+++ reactapp/src/card.jsx
@@ -5,7 +5,9 @@ function Card()
         <div className="content">
             <img src="img.png" alt="" />
             <h1>Harsha vardhan</h1>
-           
+            <p>
+                competative programmer and mern stack developer
+            </p>
 
         </div>
     );
