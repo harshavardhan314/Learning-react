05db19dc5547de9c58beb2148788f36ed7730309
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-02 17:09:18 +0530

    created the card component

--- reactapp/src/card.jsx
+++ reactapp/src/card.jsx
@@ -2,9 +2,14 @@
 function Card()
 {
     return(
-        <header>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corporis pariatur, illo asperiores reprehenderit quas fuga. Ex facilis a obcaecati enim nihil, veniam commodi atque culpa. Necessitatibus eum sint asperiores.</p>
-        </header>
+        <div className="content">
+            <img src="img.png" alt="" />
+            <p>Harsha vardhan</p>
+            <p>
+                competative programmer and mern stack developer
+            </p>
+
+        </div>
     );
 
 }
