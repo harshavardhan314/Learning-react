aaac6142f47ddc7a31ea1c7bbb42a3edea8de9d0
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-04 18:06:57 +0530

    .

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -3,7 +3,7 @@ import Card from './card.jsx';
 function App() {
   return (
     <>
-      <Card islogin={false} name="Harsha Vardhan" stack="MERN"/>
+      <Card />
     </>
   );
 }
--- reactapp/src/card.jsx
+++ reactapp/src/card.jsx
@@ -1,22 +1,31 @@
+import PropTypes from 'prop-types';
 
-function Card(props)
-{
-    return(
-        props.islogin ?
-        <div className="content">
-            <img src="img.png" alt="" />
-            <h1>{props.name}</h1>
-            <h1>{props.stack}</h1>
-            <p>
-                competative programmer and mern stack developer
-            </p>
-
-        </div>
-        :
-        <p>hello</p>
-    
-                
+function Card(props) {
+    return (
+        props.islogin ? (
+            <div className="content">
+                <h1>{props.name}</h1>
+                <h1>{props.stack}</h1>
+                <p>
+                    Competitive programmer and MERN stack developer
+                </p>
+            </div>
+        ) : (
+            <p>Hello</p>
+        )
     );
-
 }
+
+Card.propTypes = {
+    islogin: PropTypes.bool,
+    name: PropTypes.string,
+    stack: PropTypes.string
+};
+
+Card.defaultProps = {
+    islogin: true,
+    name: "harsha",
+    stack: "MERN Stack"
+};
+
 export default Card;
