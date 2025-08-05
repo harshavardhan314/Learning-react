dcdf9d26fdaba9a54d828ea59f6892fe73172700
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-05 15:56:45 +0530

    .

--- reactapp/src/List.jsx
+++ reactapp/src/List.jsx
@@ -2,9 +2,14 @@
 
 function List()
 {
-    const arr=["apple","pineapple","banana"]
-    const ele=arr.map(arr=><li>{arr}</li>)
-    return(<ul>{ele}</ul>)
+    const arr=[{name:"apple" ,id:1,calories:30},{name:"pineapple",id:2,calories:40},{name:"banana",id:3,calories:50}]
+    
+
+    arr.sort((a,b)=>b.calories-a.calories)
+   const ele=arr.map(arr=><li key={arr.calories}>
+    {arr.calories} :&nbsp; {arr.name}
+   </li>)
+   return(<ul>{ele}</ul>)
 }
 
 export default List
