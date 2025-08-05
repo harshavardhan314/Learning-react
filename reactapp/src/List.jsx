1c77059cad8c87b3b4e1831dc4068074d5afd13d
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-05 16:12:26 +0530

    Update List.jsx

--- reactapp/src/List.jsx
+++ reactapp/src/List.jsx
@@ -2,14 +2,21 @@
 
 function List()
 {
-    const arr=[{name:"apple" ,id:1,calories:30},{name:"pineapple",id:2,calories:40},{name:"banana",id:3,calories:50}]
+    const arr=[{name:"apple" ,id:1,calories:100},{name:"pineapple",id:2,calories:40},{name:"banana",id:3,calories:50}]
     
 
-    arr.sort((a,b)=>b.calories-a.calories)
-   const ele=arr.map(arr=><li key={arr.calories}>
-    {arr.calories} :&nbsp; {arr.name}
-   </li>)
-   return(<ul>{ele}</ul>)
+   const lowcal=arr.filter(arr=>arr.calories<100)
+   return (
+  <ul>
+        {lowcal.map(item => (
+                <li key={item.id}>
+                    {item.name} - {item.calories}
+                </li>
+        ))}
+  </ul>
+);
+
+   
 }
 
 export default List
