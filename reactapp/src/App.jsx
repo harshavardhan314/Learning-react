ba4a7c3fa908a29bbe11abf4aa5574292e7335b2
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-05 16:19:10 +0530

    .

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     
-          <List/>
+          <List items={arr}/>
     </>
   );
 }
--- reactapp/src/List.jsx
+++ reactapp/src/List.jsx
@@ -3,10 +3,10 @@
 function List(props)
 {
     const item=props.items
-   const lowcal=arr.filter(arr=>arr.calories<100)
+   
    return (
   <ul>
-        {lowcal.map(item => (
+        {item.map(item => (
                 <li key={item.id}>
                     {item.name} - {item.calories}
                 </li>
