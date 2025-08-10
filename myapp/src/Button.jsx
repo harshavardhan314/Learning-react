c3bc9b4594631dcb7284b28b7be88a1025a7dc40
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-10 11:16:03 +0530

    .

--- myapp/src/Button.jsx
+++ myapp/src/Button.jsx
@@ -1,6 +1,17 @@
 function Button() {
+
+    // when we pass an argument we have enclose the callback in a function other wise by default it will invoke the function.call(this, args)
+    const click=(name)=>console.log(`${name}`)
+
+    // without parameter
+    /*
+    const click=()=>console.log("hello")
+ return (
+        <button onClick={click()}>click me</button>
+    );
+    */
     return (
-        <button>click me</button>
+        <button onClick={ ()=>click("HARSHA")}>click me</button>
     );
 }
 export default Button;
