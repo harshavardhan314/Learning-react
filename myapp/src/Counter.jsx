7a2e0f686b6ca3464b68609368ab5f01f37cca9f
Harsha_314 <hharshavardhan974@gmail.com>
2025-08-12 16:24:57 +0530

    Update Counter.jsx

--- myapp/src/Counter.jsx
+++ myapp/src/Counter.jsx
@@ -10,7 +10,7 @@ function Counter() {
     const Increase = () => {
         setCount(count=>count + 1);
         setCount(count=>count + 1);
-        setCount(count=>count + 1);
+       
     };
 
     const Reset = () => {
