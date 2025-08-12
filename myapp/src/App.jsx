60b9dd8a34530da9de6751a73838ae331800dc9a
Harsha_314 <hharshavardhan974@gmail.com>
2025-08-12 15:11:53 +0530

    Created counter component

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,15 @@
-import Button from './Button';
-import ProfilePic from './profilepic';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Counter'
+
 function App() {
-    return (
-       
-            <ProfilePic/>
-    );
+  const [count, setCount] = useState(0)
+
+  return (
+    <Counter></Counter>
+  )
 }
 
-export default App;
+export default App
---
+++ myapp/src/Counter.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const Decrease = () => {
+        setCount(count - 1);
+    };
+
+    const Increase = () => {
+        setCount(count + 1);
+    };
+
+    const Reset = () => {
+        setCount(0);
+    };
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={Decrease}>Decrease</button>
+            <button onClick={Increase}>Increase</button>
+            <button onClick={Reset}>Reset</button>
+        </div>
+    );
+}
+
+export default Counter;
