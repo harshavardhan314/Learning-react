0d52339bdaf03d7d545e8b1aa06ed4ae3bf8b668
harshavardhan314 <hharshavardhan974@gmail.com>
2025-08-23 20:04:30 +0530

    Update Array.jsx

--- myapp/src/Array.jsx
+++ myapp/src/Array.jsx
@@ -2,11 +2,24 @@
 
 function Array()
 {
-    
-    return(
-        <h1>
-            hii
-        </h1>
-    )
+
+    const fruits = ["Apple", "Banana", "Mango", "Orange",
+        {
+            name: "Grapes",
+            color: "Purple",
+            taste: "Sweet"
+        }
+    ];
+
+  return (
+    <div>
+      <h1>Fruit List</h1>
+      <ul>
+        {
+            fruits.map((item)=>)
+        }
+      </ul>
+    </div>
+  );
 }
 export default Array;
