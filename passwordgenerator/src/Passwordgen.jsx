b513dff131afd7495ff2418a8876bb409103b630
harshavardhan314 <hharshavardhan974@gmail.com>
2025-10-06 22:05:12 +0530

    hands on with hooks

--- passwordgenerator/src/Passwordgen.jsx
+++ passwordgenerator/src/Passwordgen.jsx
@@ -1,48 +1,94 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
-const Passwordgen = () => {
-    const [length,setLength]=useState(6)
+const PasswordGen = () => {
+  const [length, setLength] = useState(8)
+  const [number, setNumber] = useState(false)
+  const [alpha, setAlpha] = useState(false)
+  const [password, setPassword] = useState('')
+  const [display, setDisplay] = useState(false)
+
+  const passref=useRef()
+
+  const generator = useCallback(() => {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    const numbers = "0123456789"
+    const symbols = "@#$&"
+
+    if (number) chars += numbers
+    if (alpha) chars += symbols
+
+    let res = ""
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length)
+      res += chars[randomIndex]
+    }
+
+    setPassword(res)
+  }, [length, alpha, number])
+
+  useEffect(() => {
+    generator()
+  }, [generator])
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password)
+    setDisplay(true)
+    passref.current.select()
+    setTimeout(() => setDisplay(false), 2000)
+  }
+
   return (
-    <div className="pass-container bg-black w-full h-lvh flex justify-center items-center">
-      <div className="password-generator bg-blue-300 w-[400px] p-6 rounded-2xl shadow-lg">
-        
-        <h1 className="text-center text-2xl font-bold mb-6 text-black">
-          Password Generator
-        </h1>
+    <div className="password-generator h-lvh bg-blue-900 text-white p-5">
+      {display && (
+        <h1 className="text-green-400 text-2xl mb-3">Copied successfully!</h1>
+      )}
 
-        {/* input with copy button */}
-        <div className="relative mb-6">
+      <h1 className="text-4xl mb-5">Password Generator</h1>
+
+      <div className="input-box">
+        <input
+          type="text"
+          value={password}
+          readOnly
+          className="bg-white text-black border-none p-2.5 w-80 rounded-2xl"
+          ref={passref}
+        />
+        <button
+          className="bg-blue-500 p-3 rounded-xl cursor-pointer ml-3"
+          onClick={handleCopy}
+        >
+          Copy
+        </button>
+
+        <div className="options mt-5 ml-5">
           <input
-            type="text"
-            className="bg-white w-full p-3 pr-16 rounded-2xl outline-none text-black "
-            placeholder="password"
-          />
-          <button
-            className="shrink-0 bg-black text-white px-3 py-1 rounded-xl text-sm"
-          >
-            Copy
-          </button>
-        </div>
-
-        {/* options */}
-        <div className="flex flex-col gap-2 text-black">
-          <div>
-            <input type="range" 
-            min={6}
+            type="range"
+            min={8}
             max={20}
             value={length}
-            onChange={(e)=>{setLength(e.target.value)}}
+            className="cursor-pointer"
+            onChange={(e) => setLength(Number(e.target.value))}
+          />
+          <label className="text-2xl text-orange-500 m-5">
+            Length: {length}
+          </label>
+
+          <div className="mt-3">
+            <input
+              type="checkbox"
+              className="cursor-pointer"
+              checked={number}
+              onChange={() => setNumber((prev) => !prev)}
             />
-            <span>Length: {length}</span>
-            <input type="number" className="w-16 p-1 ml-2 rounded-md outline-none" min="4" max="20" />
-          </div>
-          <div>
-            <label className="mr-4">
-              <input type="checkbox" className="mr-2" /> Numbers
-            </label>
-            <label>
-              <input type="checkbox" className="mr-2" /> Characters
-            </label>
+            <label className="text-2xl text-orange-500 m-5">Number</label>
+
+            <input
+              type="checkbox"
+              className="cursor-pointer"
+              checked={alpha}
+              onChange={() => setAlpha((prev) => !prev)}
+            />
+            <label className="text-2xl text-orange-500 m-5">Character</label>
           </div>
         </div>
       </div>
@@ -50,4 +96,4 @@ const Passwordgen = () => {
   )
 }
 
-export default Passwordgen
+export default PasswordGen
